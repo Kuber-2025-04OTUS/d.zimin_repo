34c2754ab8bb89223ec48a441000a9574ecbdb0f
d.zimin <d.zimin@21vek.by>
2025-06-22 16:15:09 +0300

    ДЗ-10

---
+++ kubernetes-gitops/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+NAMESPACE="argo-cd"
+
+if [ "$1" = "i" ]; then
+    # https://github.com/argoproj/argo-helm/blob/main/charts/argo-cd/values.yaml
+    helm repo add argo https://argoproj.github.io/argo-helm 2>/dev/null || true
+    helm repo update
+    helm upgrade --install argocd argo/argo-cd \
+      --namespace "$NAMESPACE" --create-namespace \
+      --values "values.yml"
+
+    # https://github.com/kubernetes/ingress-nginx
+    helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+    helm repo update
+    helm upgrade --install nginx-ingress ingress-nginx/ingress-nginx --version 4.11.5 \
+      --namespace ingress-nginx \
+      --create-namespace
+elif [ "$1" = "u" ]; then
+    helm uninstall argocd -n "$NAMESPACE"
+    helm uninstall nginx-ingress -n
+fi
---
+++ kubernetes-gitops/label.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+kubectl label nodes cl1s78mdste1iplv67cf-ifyh homework=true --overwrite
